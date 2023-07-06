025246db32f85ad64c47f1db58a26020243de028
Gaston Herrlein <herrleingaston@gmail.com>
2023-07-06 23:48:25 +0200

    Se mejora el estilo dinamico de las secciones de la ruleta. Por alguna razon no lee los estilos inline del contenedor roulette-section-container

--- src/components/RouletteServiceContainer.jsx
+++ src/components/RouletteServiceContainer.jsx
@@ -10,58 +10,50 @@ const projectList = (url) => {
   return axios.get("http://localhost:" + url);
 };
 
-const radioCalc = (list) =>{
-  let radio = Math.round(360/list.length)
-  return (radio*list.length <= 360) ? radio : (radio-1)
-}
+const radioCalc = (list) => {
+  let radio = Math.round(360 / list.length);
+  return radio * list.length <= 360 ? radio : radio - 1;
+};
 
 const generationRotationStyle = (radio, index) => {
-  
-  let rotation = radio*index
-  let deformation = -(90-radio)
-  let rotatinStyle = 'rotate(' + rotation + 'deg) skewY(' + deformation + 'deg)'
+  let rotation = radio * index;
+  let deformation = -(90 - radio);
+  let rotatinStyle =
+    "rotate(" + rotation + "deg) skewY(" + deformation + "deg)";
   return {
-    "transform": rotatinStyle,
-    "WebkitTransform": rotatinStyle,
-    "MozTransform": rotatinStyle,
-    "msTransform": rotatinStyle, 
-    "OTransform": rotatinStyle
-  }
-}
-
+    transform: rotatinStyle,
+    WebkitTransform: rotatinStyle,
+    MozTransform: rotatinStyle,
+    msTransform: rotatinStyle,
+    OTransform: rotatinStyle,
+  };
+};
 
 export const RouletteSectionContainer = () => {
   const [list, setList] = useState([]);
-  const [radio, setRadio] = useState (360);
+  const [radio, setRadio] = useState(360);
 
   useEffect(() => {
     projectList(url)
       .then((response) => {
-        setList(response.data)
-        setRadio (radioCalc(response.data))  
+        setList(response.data);
+        setRadio(radioCalc(response.data));
       })
       .catch((error) => console.error());
   }, []);
 
   return (
     <div id="roulette">
-      {list.map ((project, index) => {
+      {list.map((project, index) => {
         return (
-         <RouletteSection key={index} section={project} rotation={generationRotationStyle(radio, index)}/>
-        )
+          <RouletteSection
+            key={index}
+            section={project}
+            rotation={generationRotationStyle(radio, index)}
+            list={list}
+          />
+        );
       })}
-      {/* <RouletteSection section={"Borrar tu codigo"} />
-      <RouletteSection section={"Apagar la computadora"} />
-      <RouletteSection section={"Formatear computadora"} />
-      <RouletteSection section={"Compra helado"} />
-      <RouletteSection section={"Toma descanso"} />
-      <RouletteSection section={"Huevo en tu cabeza"} />
-      <RouletteSection section={"Tira la laptop"} />
-      <RouletteSection section={"$1,00USD a tus subs"} />
-      <RouletteSection section={"Comer snack"} />
-      <RouletteSection section={"Jugar un rato"} />
-      <RouletteSection section={"!Terminar tu tarea¡"} />
-      <RouletteSection section={"Vuelve a grabar"} /> */}
     </div>
   );
 };
--- src/components/roulette/RouletteSection.jsx
+++ src/components/roulette/RouletteSection.jsx
@@ -1,9 +1,73 @@
-export const RouletteSection = ({section, rotation}) => {
-    return (
-      <div className="roulette-section" style={rotation}>
-        <div className="roulette-section-container">
-          <p>{section}</p>
-        </div>
+const styles = [
+  {
+    transform: "skewY(0deg) rotate (52deg)",
+    WebkitTransform: "skewY(0deg) rotate (52deg)",
+    MozTransform: "skewY(0deg) rotate (52deg)",
+    msTransform: "skewY(0deg) rotate (52deg)",
+    OTransform: "skewY(0deg) rotate (52deg)",
+  },
+  {
+    transform: "skewY(15deg) rotate (40deg)",
+    WebkitTransform: "skewY(15deg) rotate (40deg)",
+    MozTransform: "skewY(15deg) rotate (40deg)",
+    msTransform: "skewY(15deg) rotate (40deg)",
+    OTransform: "skewY(15deg) rotate (40deg)",
+  },
+  {
+    transform: "skewY(30deg) rotate (30deg)",
+    WebkitTransform: "skewY(30deg) rotate (30deg)",
+    MozTransform: "skewY(30deg) rotate (30deg)",
+    msTransform: "skewY(30deg) rotate (30deg)",
+    OTransform: "skewY(30deg) rotate (30deg)",
+  },
+  {
+    transform: "transform: skewY(39deg) rotate (27deg)",
+    WebkitTransform: "transform: skewY(39deg) rotate (27deg)",
+    MozTransform: "transform: skewY(39deg) rotate (27deg)",
+    msTransform: "transform: skewY(39deg) rotate (27deg)",
+    OTransform: "transform: skewY(39deg) rotate (27deg)",
+  },
+  {
+    transform: "transform: skewY(39deg) rotate (27deg)",
+    WebkitTransform: "transform: skewY(39deg) rotate (27deg)",
+    MozTransform: "transform: skewY(39deg) rotate (27deg)",
+    msTransform: "transform: skewY(39deg) rotate (27deg)",
+    OTransform: "transform: skewY(39deg) rotate (27deg)",
+  },
+  {
+    transform: "skewY(50deg) rotate (22deg)",
+    WebkitTransform: "skewY(50deg) rotate (22deg)",
+    MozTransform: "skewY(50deg) rotate (22deg)",
+    msTransform: "skewY(50deg) rotate (22deg)",
+    OTransform: "skewY(50deg) rotate (22deg)",
+  },
+  {
+    transform: "skewY(54deg) rotate (20deg)",
+    WebkitTransform: "skewY(54deg) rotate (20deg)",
+    MozTransform: "skewY(54deg) rotate (20deg)",
+    msTransform: "skewY(54deg) rotate (20deg)",
+    OTransform: "skewY(54deg) rotate (20deg)",
+  },
+  {
+    transform: "skewY(50deg) rotate (17deg)",
+    WebkitTransform: "skewY(50deg) rotate (17deg)",
+    MozTransform: "skewY(50deg) rotate (17deg)",
+    msTransform: "skewY(50deg) rotate (17deg)",
+    OTransform: "skewY(50deg) rotate (17deg)",
+  },
+];
+
+const generationRotationContainerStyle = (styles, list) => {
+  return styles[list.length - 4];
+};
+
+
+export const RouletteSection = ({ section, rotation, list }) => {
+  return (
+    <div className="roulette-section" style={rotation}>
+      <div className="roulette-section-container" style={generationRotationContainerStyle (styles, list)}>
+        <p>{section}</p>
       </div>
-    );
-  };
+    </div>
+  );
+};
